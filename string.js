744b6664e24e15cda31f1e25653176b7a49ba045
tigerdev <patinyatiger1552@gmail.com>
2021-10-29 22:29:23 +0700

    ทบทวน String

---
+++ string.js
@@ -0,0 +1,9 @@
+// ทำงานกับ String
+
+let customerName = "Devrogerstck"
+let tel = "0933631728"
+let city = "อำนาจเจริญ"
+
+const address = `ชื่อลูกค้า : ${customerName} ที่อยู่ : ${city} เบอร์ติตค่อ : ${tel}`
+
+console.log(address)
