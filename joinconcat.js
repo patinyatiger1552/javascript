95f4457435d008f97a4e782d2569b80c20a07576
tigerdev <patinyatiger1552@gmail.com>
2021-10-30 19:01:57 +0700

    ทบทวน joinconcat

---
+++ joinconcat.js
@@ -0,0 +1,17 @@
+// join concat
+
+// join การทำงาน
+const data = [100,200,300]
+const result = data.join()
+// console.log(data)
+// console.log(data.join())
+
+// concat การทำงาน
+
+const data1 = [100,200,300]
+const data2 = [400,500,600]
+
+const allData = data1.concat(data2)
+
+console.log(allData)
+
